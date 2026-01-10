2a2b88001b36438c0771f2f46fb8d14cd895be36
Kingofpottatoes <morelnathan1511@gmail.com>
2026-01-11 00:27:15 +0100

    Implement movement logic in BrawlerPlayed class to handle stun and root states

--- Simulator/Class/Class_Brawler_Played.py
+++ Simulator/Class/Class_Brawler_Played.py
@@ -1,3 +1,4 @@
+from Class_Map import game_map
 class BrawlerPlayed:
     def __init__(
         self,
@@ -51,29 +52,16 @@ class BrawlerPlayed:
 
         # Combat
         self.Can_attack = Can_attack
-class BrawlerPlayed:
-    def __init__(
-            self,
-            coords,#tuple x,y
-            deplacement,#(vx,vy)
-            HP,#int
-            max_Hp,#int
-            Speed,#int
-            ReloadSpeed, #nmuni/s
-            Muni,#float compris entre 0 et 1
-            State,#[(State,duration_left),...]
-            SP,#[indice du sp,Buffed or not]
-            Gadjet,#[indice du gadjet,Buffed or not]
-            Gear1,#[indice du gear]
-            Gear2,#[indice du gear] Gear1!=Gear2
-            Hyper,#(A l'hyper ou pas,Buffed or not)
-            Ult_charge,#float 0>1
-            Ult_Uses,#int
-            Can_attack,#bool
-            
+        # Apparence
+        self.Texture = Texture
 
-            
-
-
-    ):
-        self.coords=coords
+        def try_move(self,deplacement):
+            if 'stun' in [state[0] for state in self.State] or 'root' in [state[0] for state in self.State]:
+                return self.coords
+            else:
+                self.coords = (self.coords[0] + self.deplacement[0], self.coords[1])
+                if game_map.collides(self):
+                    self.coords = (self.coords[0] - self.deplacement[0], self.coords[1])
+                self.coords = (self.coords[0], self.coords[1] + self.deplacement[1])
+                if game_map.collides(self):
+                    self.coords = (self.coords[0], self.coords[1] - self.deplacement[1])
