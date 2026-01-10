41c05b0189c468a95d969cb09b3048fb4dadb7e6
Kingofpottatoes <morelnathan1511@gmail.com>
2026-01-11 00:27:25 +0100

    Refactor GameMap class to Map and update is_walkable method for clarity

--- Simulator/Class/Class_Map.py
+++ Simulator/Class/Class_Map.py
@@ -1,8 +1,9 @@
 from Class_tiles import Tile
-class GameMap:
+from Colision import *
+class Map:
 
     def __init__(self, map_data, tiles):
-        self.map_data = map_data      # tableau de chars
+        self.map_data = map_data      # tableau de characteres
         self.tiles = tiles            # dict id -> Tile
         self.height = len(map_data)
         self.width = len(map_data[0])
@@ -12,7 +13,7 @@ class GameMap:
     def tile_at(self, x, y):
         return self.tiles[self.map_data[y][x]]
 
-    def is_walkable_circle(self, x, y, hitbox):
+    def is_walkable(self, x, y, hitbox):
         # Tiles potentiellement touchées
         min_x = int(x - hitbox)
         max_x = int(x + hitbox)
@@ -22,8 +23,9 @@ class GameMap:
         for ty in range(min_y, max_y + 1):
             for tx in range(min_x, max_x + 1):
                 tile = self.tile_at(tx, ty)
-                if tile and tile.blocks_movement:
-                    if circle_intersects_tile(x, y, hitbox, tx, ty):
+                if tile and tile.blocks_movement: #verifie que la tile existe et bloque les mouvements
+                    if circle_intersects_tile(x, y, hitbox,tx,ty):
                         return False
         return True
+    
     
