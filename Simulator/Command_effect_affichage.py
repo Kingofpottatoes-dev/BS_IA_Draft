7cf2f24f4ea254d188f2f33b7f16dba56b1743ba
Kingofpottatoes-dev <morelnathan1511@gmail.com>
2026-01-17 23:56:06 +0100

    create separated file to list all command possible in pygame cmd

--- Simulator/Command_effect_affichage.py
+++ Simulator/Command_effect_affichage.py
@@ -1,8 +1,16 @@
-def break_tile(x, y):
-    name, grid = maps[current_map_index]
+import os
+import  sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "Class"))
+from Class.Class_Game_State import GameState
+from pygame import current_maps
+def break_tile(x, y,):
+    name, grid = current_maps
     if 0 <= y < len(grid) and 0 <= x < len(grid[y]):
         grid[y][x] = "."  # vide
 def place_tile(x, y, tile_char):
-    name, grid = maps[current_map_index]
+    name, grid = current_maps
     if 0 <= y < len(grid) and 0 <= x < len(grid[y]):
         grid[y][x] = tile_char
+def Start_Game():
+    game_state = GameState(map=current_maps[1], mode="")
+    return game_state
